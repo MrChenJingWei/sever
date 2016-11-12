15423ba9bf4df0295682ce2051936e1d42ff1566
dorayo <yomingyo@gmail.com>
2016-11-12 09:29:57 +0800

    init

---
+++ server.js
@@ -0,0 +1,14 @@
+/**
+ * Created by qingyun on 16/9/25.
+ */
+
+var express = require('express');
+var path = require('path');
+
+var app = express();
+
+app.use(express.static(path.join(__dirname, 'public'))); //配置静态文件路径
+
+app.listen(80,function () {
+    console.log("http://localhost:80");
+});
